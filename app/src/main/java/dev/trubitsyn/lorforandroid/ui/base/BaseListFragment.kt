ba26c40426095645276d725cd4d710c7334e076a
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2021-01-30 17:54:12 +0300

    Do not retain instance (unsupported by Hilt)

--- app/src/main/java/dev/trubitsyn/lorforandroid/ui/base/BaseListFragment.kt
+++ app/src/main/java/dev/trubitsyn/lorforandroid/ui/base/BaseListFragment.kt
@@ -19,7 +19,6 @@ package dev.trubitsyn.lorforandroid.ui.base
 
 import android.os.Bundle
 import android.view.*
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.ViewFlipper
 import androidx.fragment.app.Fragment
@@ -45,7 +44,6 @@ abstract class BaseListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-        retainInstance = true
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
